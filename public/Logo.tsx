dfd11453f8715e7ef887648b9dd72835a17dfc16
joshua-indev <joshuag@startupmedia.uk>
2025-09-08 16:30:11 +0100

    feat: navbar

---
+++ public/Logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+import logo from "@/public/images/ng-logo (4).png";
+
+export const Logo = () => <Image src={logo.src} alt="Nerissa Golden Logo" width={180} height={180} />;
